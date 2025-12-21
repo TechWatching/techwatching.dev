c11ab705302b17f14a3d09cbc2885165ddffd16d
Alexandre Nedelec <15186176+TechWatching@users.noreply.github.com>
2025-12-21 23:12:05 +0100

    chore: remove all files to start from latest Nuxt SaaS template version

--- server/routes/dotnet_feed.rss.ts
+++
@@ -1,7 +0,0 @@
-﻿import {generateFeed} from "~/server/feed";
-
-export default defineEventHandler(async (event) => {
-  const feed = await generateFeed(event, [".NET"])
-  appendHeader(event, 'Content-Type', 'application/xml')
-  return feed.rss2()
-})
