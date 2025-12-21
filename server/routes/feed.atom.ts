000716f1277dc0a4803c9cf10f9df16414d56046
Alexandre Nedelec <15186176+TechWatching@users.noreply.github.com>
2025-12-21 23:12:05 +0100

    chore: remove all files to start from latest Nuxt SaaS template version

--- server/routes/feed.atom.ts
+++
@@ -1,7 +0,0 @@
-﻿import {generateFeed} from "~/server/feed";
-
-export default defineEventHandler(async (event) => {
-  const feed = await generateFeed(event)
-  appendHeader(event, 'Content-Type', 'application/xml')
-  return feed.atom1()
-})
--- utils/tag.ts
+++
@@ -1,5 +0,0 @@
-﻿import kebabCase from "just-kebab-case";
-
-export const getTagSlug = (tag: string) => kebabCase(tag.toLowerCase().replace('/', ''))
-
-export const getTagRoute = (tag: string) => `/tags/${getTagSlug(tag)}`
