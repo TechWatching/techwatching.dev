ad910f910b7302a53051a9ac1c9f3c392a6bb6ff
Alexandre Nedelec <15186176+TechWatching@users.noreply.github.com>
2025-12-21 23:17:19 +0100

    feat: intialize website from Nuxt SaaS template
    commit id: d548b61351af9ef0802da1946d26b2c35a3db449

---
+++ nuxt.config.ts
@@ -0,0 +1,43 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+    '@nuxt/hints',
+    '@nuxt/scripts'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ],
+      crawlLinks: true
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
